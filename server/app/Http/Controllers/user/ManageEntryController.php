8c4fc4c5da35dc4e64fb35ad3a5f80ed8e3e76e0
enteitsuki <enteitsuki@gmail.com>
2021-03-30 12:53:53 +0900

    記事編集・削除機能作成

--- server/app/Http/Controllers/user/ManageEntryController.php
+++ server/app/Http/Controllers/user/ManageEntryController.php
@@ -44,17 +44,67 @@ class ManageEntryController extends Controller
 	
 		return redirect("home")->withStatus("記事を作成しました");
     }
-    
+
 	/* 記事の編集 */
-	function showEditForm(){
-		return view("news.edit_form");
+	function showEditForm($id)
+	{
+		$user = Auth::user();
+		$news = $user->newsEntry()->find($id);
+
+		if (!$news) {
+			return redirect("home")->withStatus("記事がありません");
+		}
+
+		return view("news.edit_form", [
+			"news" => $news
+		]);
 	}
-	function update(Request $request, $id){
-		//@TODO 記事の更新
+
+	function update(Request $request, $id)
+	{
+		$user = Auth::user();
+		$news = $user->newsEntry()->find($id);
+
+		if (!$news) {
+			return redirect("home")->withStatus("記事がありません");
+		}
+		/* 入力値の受け取り */
+
+		$input = $request->only('title', 'description', 'body');
+
+		$validator = Validator::make($input, [
+			'title' => 'required|string|max:200',
+			'description' => 'string|max:200',
+			'body' => 'required|string'
+		]);
+		//バリデーション失敗
+		if ($validator->fails()) {
+			return redirect('news/edit/' . $news->id)
+				->withErrors($validator)
+				->withInput();
+		}
+
+		//バリデーション成功
+		$news->title = $input["title"];
+		$news->description = $input["description"];
+		$news->body = $input["body"];
+		$news->user_id = Auth::id();
+		$news->thumbnail_url = "";
+		$news->image_url = "";
+		$news->save();
+
 		return redirect("home")->withStatus("記事を更新しました");
 	}
+
 	/* 記事の削除 */
-	function delete($id){
+	function delete($id)
+	{
+		$user = Auth::user();
+		$news = $user->newsEntry()->find($id);
+		if (!$news) {
+			return redirect("home")->withStatus("記事がありません");
+		}
+		$news->delete();
 		return redirect("home")->withStatus("記事を削除しました");
 	}
 }
